cda79e009863731cb06a1b3451dd3f2e339e49f4
amany9000 <201651007@iiitvadodara.ac.in>
2018-08-11 21:05:25 +0530

    Added contracts.sol

---
+++ contracts/inbox.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.17;
+
+contract Inbox {
+    string public message;
+    constructor(string initialMssg) public {
+        message = initialMssg;
+    }
+    function setMssg(string newMssg) public returns (string){
+        message = newMssg;
+    } 
+}
