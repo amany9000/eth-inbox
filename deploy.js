9f5fd54c6044705a2b14c4bc0a5c2ce0fd515a36
amany9000 <201651007@iiitvadodara.ac.in>
2018-08-14 14:20:10 +0530

    Deployed to Rinkeby Ethereum Network

---
+++ deploy.js
@@ -0,0 +1,24 @@
+
+const hdWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const provider = new hdWalletProvider(
+	"dust tired lawn post else ladder forest memory quality click obey hurt",
+	"https://rinkeby.infura.io/v3/a28609fdb48f468e9ca4607b4ff95140"
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+	const accounts = await  web3.eth.getAccounts();
+	
+	console.log("Attempting to deploy from account ", accounts[0]);
+
+	const result = await new web3.eth.Contract(JSON.parse(interface))
+		.deploy({data: "0x" + bytecode, arguments: ["Hey"]})
+		.send({gas: "1000000", from: accounts[0]});
+
+	console.log("Bazinga!!!, Deployed at "result.options.address);	
+}  
+deploy();
